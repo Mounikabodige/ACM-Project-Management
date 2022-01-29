26b9641006a9277451a2b8f5f855f85de803a1b9
Mounika Bodige <M.Bodige@medpace.medpace.com>
2022-01-28 20:59:04 -0500

    Added new methods.

--- ACM Project Management.Bussinesslayer/BussinessLogicServices/CustomerService.cs
+++ ACM Project Management.Bussinesslayer/BussinessLogicServices/CustomerService.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using ACM_Project_Management.Bussinesslayer.Models;
+using System;
 namespace ACM_Project_Management.Bussinesslayer.BussinessLogicServices
 {
     public class CustomerService
     {
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        /// <param name="First"></param>
+        /// <param name="Last"></param>
+        /// <returns></returns>
         public string FullName(string First, string Last)
         {
             if(!string.IsNullOrWhiteSpace(First) && !string.IsNullOrWhiteSpace(Last))
@@ -21,6 +28,40 @@ namespace ACM_Project_Management.Bussinesslayer.BussinessLogicServices
             {
                 return "";
             }
+        }
+
+        /// <summary>
+        /// Retreive a customer based on the customer identifier.
+        /// </summary>
+        /// <param name="CustomerId"></param>
+        /// <returns></returns>
+        public Customer Retrieve(int CustomerId)
+        {
+            var customer = new Customer();
+            return customer;
+        }
+
+        /// <summary>
+        /// Saves the customer.
+        /// </summary>
+        /// <returns> True if customer is saved; else false.</returns>
+        public bool Save()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the customer data.
+        /// </summary>
+        /// <returns> True if customer is valid; else false.</returns>
+        public bool Validate(string nameOrEmail)
+        {
+            var isValid = false;
+
+            if(string.IsNullOrWhiteSpace(nameOrEmail))
+                isValid = false;
+
+            return isValid;
         }
     }
 }
